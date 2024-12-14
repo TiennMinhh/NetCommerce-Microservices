9543c8ac34f15effb69b83b4d5488f21c2d83db5
HOANG Minh Tien <tinhoang7901@gmail.com>
2024-12-14 11:42:33 +0700

    Discount: Initial database use SqlLite

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,7 +1,12 @@
+using Discount.Grpc.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<DiscountContext>(opts =>
+        opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 
 var app = builder.Build();
 
